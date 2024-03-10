ec67ef1fb9b1bf0e1789fe496bf7a1176cadf04e
andprov <abpronyaev@gmail.com>
2024-03-10 21:10:58 +0300

    refactor:

--- app/__main__.py
+++ app/__main__.py
@@ -4,15 +4,16 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
-from middlewares.access import PrivateMiddleware
 from redis.asyncio import Redis
-from services.user_service import user_service
+
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from app import config
 from app.handlers import auto, menu, search, user
+from app.middlewares.access import PrivateMiddleware
 from app.middlewares.db_session import DbSessionMiddleware
 from app.misc.ui_commands import set_ui_commands
+from app.services.user_service import user_service
 
 
 async def main():
