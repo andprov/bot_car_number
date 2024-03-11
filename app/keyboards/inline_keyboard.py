6936c2a3408ef02cab299754e232abf4101e23fb
andprov <abpronyaev@gmail.com>
2024-03-11 18:33:34 +0300

    refactor:

--- app/keyboards/inline_keyboard.py
+++ app/keyboards/inline_keyboard.py
@@ -2,15 +2,6 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from app.misc import cmd
 
-BACK_TXT = "<< Назад"
-DEL_CONFIRM_TXT = "Подтвердить Удаление"
-USER_MENU_TXT = "Мои Данные"
-AUTO_MENU_TXT = "Мои Автомобили"
-SEARCH_TXT = "Поиск"
-ADD_TXT = "Добавить"
-DELETE_TXT = "Удалить"
-SAVE_TXT = "Сохранить"
-
 
 def get_btn(text: str, action: str) -> InlineKeyboardButton:
     """Вернуть кнопку."""
@@ -19,22 +10,22 @@ def get_btn(text: str, action: str) -> InlineKeyboardButton:
 
 def back_kb(back: str) -> InlineKeyboardMarkup:
     """Клавиатура возврата в предыдущее меню."""
-    back_btn = get_btn(BACK_TXT, back)
+    back_btn = get_btn(cmd.BACK_TXT, back)
     return InlineKeyboardMarkup(inline_keyboard=[[back_btn]])
 
 
 def save_kb(back: str) -> InlineKeyboardMarkup:
     """Клавиатура сохранения."""
-    save_btn = get_btn(SAVE_TXT, cmd.AUTO_SAVE)
-    back_btn = get_btn(BACK_TXT, back)
+    save_btn = get_btn(cmd.SAVE_TXT, cmd.AUTO_SAVE)
+    back_btn = get_btn(cmd.BACK_TXT, back)
     return InlineKeyboardMarkup(inline_keyboard=[[save_btn], [back_btn]])
 
 
 def main_kb() -> InlineKeyboardMarkup:
     """Клавиатура главного меню."""
-    user_menu_btn = get_btn(USER_MENU_TXT, cmd.USER)
-    auto_menu_btn = get_btn(AUTO_MENU_TXT, cmd.AUTO_MENU)
-    search_btn = get_btn(SEARCH_TXT, cmd.SEARCH)
+    user_menu_btn = get_btn(cmd.USER_MENU_TXT, cmd.USER)
+    auto_menu_btn = get_btn(cmd.AUTO_MENU_TXT, cmd.AUTO_MENU)
+    search_btn = get_btn(cmd.SEARCH_TXT, cmd.SEARCH)
     return InlineKeyboardMarkup(
         inline_keyboard=[[user_menu_btn, auto_menu_btn], [search_btn]]
     )
@@ -42,16 +33,16 @@ def main_kb() -> InlineKeyboardMarkup:
 
 def confirm_del_kb(delete: str, back: str) -> InlineKeyboardMarkup:
     """Клавиатура подтверждения удаления."""
-    delete_btn = get_btn(DEL_CONFIRM_TXT, delete)
-    back_btn = get_btn(BACK_TXT, back)
+    delete_btn = get_btn(cmd.DEL_CONFIRM_TXT, delete)
+    back_btn = get_btn(cmd.BACK_TXT, back)
     return InlineKeyboardMarkup(inline_keyboard=[[delete_btn], [back_btn]])
 
 
 def add_del_back_kb(add: str, delete: str, back: str) -> InlineKeyboardMarkup:
     """Клавиатура добавления, удаления, возврата."""
-    add_btn = get_btn(ADD_TXT, add)
-    delete_btn = get_btn(DELETE_TXT, delete)
-    back_btn = get_btn(BACK_TXT, back)
+    add_btn = get_btn(cmd.ADD_TXT, add)
+    delete_btn = get_btn(cmd.DELETE_TXT, delete)
+    back_btn = get_btn(cmd.BACK_TXT, back)
     return InlineKeyboardMarkup(
         inline_keyboard=[[add_btn, delete_btn], [back_btn]]
     )
--- app/keyboards/reply_keyboard.py
+++ app/keyboards/reply_keyboard.py
@@ -1,12 +1,14 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+from app.misc import cmd
+
 
 def contact_kb() -> ReplyKeyboardMarkup:
     """Клавиатура получения контактных данных пользователя."""
     builder = ReplyKeyboardBuilder()
     builder.row(
-        KeyboardButton(text="Отмена"),
-        KeyboardButton(text="Отправить", request_contact=True),
+        KeyboardButton(text=cmd.CANSEL_TXT),
+        KeyboardButton(text=cmd.SEND_TXT, request_contact=True),
     )
     return builder.as_markup(resize_keyboard=True)
