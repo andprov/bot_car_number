4879ec8b39579e213777b62b431d119e79e07fb8
andprov <abpronyaev@gmail.com>
2024-04-09 14:10:49 +0300

    refactor:

--- app/services/stats_service.py
+++ app/services/stats_service.py
@@ -14,6 +14,4 @@ class StatsService:
     async def check_search_access(cls, dao: StatsDAO, user_id: int) -> bool:
         """Проверить превышение лимита поисковых запросов."""
         day_search_count = await dao.get_day_search_count(user_id)
-        if day_search_count < SEARCH_COUNT_LIMIT:
-            return True
-        return False
+        return day_search_count < SEARCH_COUNT_LIMIT
